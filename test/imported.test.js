e29c3ee321d87455d7429aa9d69e488fd7275b14
sqlwwx <wwx_2012@hotmail.com>
2016-06-30 09:37:12 +0800

    test

--- test/imported.test.js
+++
@@ -1,7 +0,0 @@
-describe.skip('redis imported features', function() {
-  before(function() {
-    require('./init.js');
-  });
-
-  require('loopback-datasource-juggler/test/basic-querying.test.js');
-});
--- test/init.js
+++ test/init.js
@@ -1,9 +1,19 @@
 module.exports = require('should');
 
-var DataSource = require('loopback-datasource-juggler').DataSource;
+var juggler = require('loopback-datasource-juggler');
+
+var DataSource = juggler.DataSource;
+var ModelBuilder = juggler.ModelBuilder;
+var Schema = juggler.Schema;
 
 global.getSchema = function() {
-    var db = new DataSource(require('../'), {});
+    var db = new DataSource(require('../'), { debug: true });
     // db.log = function (a) { console.log(a); };
     return db;
 };
+
+global.getModelBuilder = function() {
+  return new ModelBuilder();
+};
+
+global.Promise = require('bluebird');
